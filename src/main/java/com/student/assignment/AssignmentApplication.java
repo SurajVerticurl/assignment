2d0b4e935c173b9f97d1d7969045c94d6e6c65f9
SurajVerticurl <144439508+SurajVerticurl@users.noreply.github.com>
2023-10-26 12:49:24 +0530

    Update AssignmentApplication.java

--- src/main/java/com/student/assignment/AssignmentApplication.java
+++ src/main/java/com/student/assignment/AssignmentApplication.java
@@ -1,4 +1,4 @@
-package com.student.assignment;
+package com.mobiles.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
