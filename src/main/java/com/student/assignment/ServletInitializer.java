abe0f291a1e099a15d3fbd4f29c5711df8574ce4
SurajVerticurl <144439508+SurajVerticurl@users.noreply.github.com>
2023-10-26 12:50:10 +0530

    Update ServletInitializer.java

--- src/main/java/com/student/assignment/ServletInitializer.java
+++ src/main/java/com/student/assignment/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.student.assignment;
+package com.mobiles.assignment;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
